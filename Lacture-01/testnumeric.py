3a3e9285e50e8cfb52b5e49467fe695cabf05258
pond12-sak25 <missamoon.sonchai@gmail.com>
2025-06-26 17:02:22 +0700

    message

---
+++ Lacture-01/testnumeric.py
@@ -0,0 +1,11 @@
+from math import pi
+
+print('%d' %100)
+print('%d' %100.58)
+print('%f' %100.587)
+print("%.2f" %100.587)
+print('%.4f' %pi)
+print('my age is %d i have %.2f bth.' %(25,3500.50))
+print('my age is' + str(25) + ' i have ' + str(3500.50)+ ' bth.')
+
+print('5+4=',5+4)
