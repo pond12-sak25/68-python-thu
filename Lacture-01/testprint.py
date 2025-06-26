473b7042dd8436d9fccd65de3e0128981b6d3c81
pond12-sak25 <missamoon.sonchai@gmail.com>
2025-06-26 16:44:48 +0700

    message

--- Lacture-01/testprint.py
+++ Lacture-01/testprint.py
@@ -1,5 +1,5 @@
 print('HELLO WORLD')
 print('Sakchai wonpromrach')
 print("I'AM HUNGRY")
-print("I'AM GOOD AND \"I WILL DO MY BEST\")
+print("I'AM GOOD AND \"I WILL DO MY BEST\"")
 print('''\ta\a\ta\ta\ta''')
